296a3c3b5206291d08c6859e246a59895eb145b7
cinta <cintatafur@gmail.com>
2023-12-12 11:36:15 +0100

    ✨  Fin de formulario reactivo dinámicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,16 +1,87 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DynamicPageComponent implements OnInit {
+  public myFormDinamic: FormGroup = this.fb.group({
+    /* Requerido y un minimo de tres letras */
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteBooks: this.fb.array([
+      ['Robert Louis Stevenson', Validators.required],
+      ['Patrick James Rothfuss', Validators.required],
+      ['John Ronald Reuel Tolkien ', Validators.required],
+      ['Brandon Sanderson', Validators.required],
+    ]),
+  });
 
-  constructor() { }
+  public newFavortite: FormControl = new FormControl('', [Validators.required]);
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {}
+
+  get favoriteBooks() {
+    return this.myFormDinamic.controls['favoriteBooks'] as FormArray;
   }
 
+  isValidField(field: string): boolean | null {
+    return (
+      this.myFormDinamic.controls[field].errors &&
+      this.myFormDinamic.controls[field].touched
+    );
+  }
+
+  isValidFieldInArray(formArray: FormArray, i: number) {
+    return formArray.controls[i].errors && formArray.controls[i].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myFormDinamic.controls[field]) return null;
+
+    const errors = this.myFormDinamic.controls[field].errors || {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
+      }
+    }
+    return null;
+  }
+
+  ngOnInit(): void {}
+
+  onAddFavorite(): void {
+    if (this.newFavortite.invalid) return;
+
+    const newBook = this.newFavortite.value;
+    this.favoriteBooks.push(this.fb.control(newBook, Validators.required));
+    this.newFavortite.reset();
+  }
+
+  onDeletFavorite(i: number): void {
+    this.favoriteBooks.removeAt(i);
+  }
+
+  onSubmit(): void {
+    /*   this.submitted = true; */
+
+    if (this.myFormDinamic.invalid) {
+      this.myFormDinamic.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myFormDinamic.value);
+    (this.myFormDinamic.controls['favoriteBooks'] as FormArray) = this.fb.array([]);
+    this.myFormDinamic.reset();
+  }
 }
